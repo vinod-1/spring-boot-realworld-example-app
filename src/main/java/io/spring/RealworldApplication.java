0f5a43d5c09d83fad6a275a2225925086f4f53ba
vinod-1 <vinod@plank.life>
2020-02-17 20:56:21 +0530

    some changes

--- src/main/java/io/spring/RealworldApplication.java
+++ src/main/java/io/spring/RealworldApplication.java
@@ -2,7 +2,7 @@ package io.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//some comment
 @SpringBootApplication
 public class RealworldApplication {
 
